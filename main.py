08e5ecd268e319e1f30a2a86af60931aa9f17878
jaroabel <jaroabel@gmail.com>
2021-04-10 21:24:15 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Beggar My Neighbor - Card Game
+#
+# 2 player game – each player in alternate turn presses the ENTER key
+# to flip a card from their deck and place in the middle deck.
+#
+# Options: --help AND --resume
+# While playing player can type ‘--help’ to get game rules and ‘--resume’ to resume the game
+# author: Jean-Marie Abel
+
+import random
+import os
+
+# function to clear screen
+
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
